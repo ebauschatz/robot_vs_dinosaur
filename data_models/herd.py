c37cd053e5603b3a48fe18cbd3e87dd3e3a7f25b
ebauschatz <eaenglish113@gmail.com>
2022-08-25 15:05:42 -0500

    feat: new classes for herds and fleets

---
+++ data_models/herd.py
@@ -0,0 +1,37 @@
+"""Class file for herds of dinosaurs
+
+Describes a group of dinosaurs that are allied in battle
+"""
+from data_models.dinosaur import Dinosaur
+
+class Herd:
+    """Represents a group of allied dinosaurs
+
+    Instance Variables:
+        dinosaurs: list:Dinosaur
+            a list of dinosaurs who join into a herd
+
+    Private Methods:
+        _generate_herd(): list:Dinosaur
+            generates a list of dinosaurs that constitute the herd
+    """
+    def __init__(self):
+        """Constructs a herd
+
+        Instance Variables:
+            dinosaurs: list:Dinosaur
+                a list of dinosaurs who join into a herd
+        """
+        self.dinosaurs = self._generate_herd()
+
+    def _generate_herd():
+        """Generates a list of dinosaurs that constitute the herd
+
+        Returns:
+            list of Dinosaur objects
+        """
+        return [
+            Dinosaur('Rosie', 25),
+            Dinosaur('K2S0', 20),
+            Dinosaur('Marvin', 15)
+        ]
